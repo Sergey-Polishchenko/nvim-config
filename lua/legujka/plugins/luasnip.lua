cc08b53e9e632af090692863aaf4ce5569dd3107
Sergey <sergeypolishchenko@gmail.com>
2025-04-05 11:47:04 +0300

    feat: add snippets for go

--- lua/legujka/plugins/luasnip.lua
+++ lua/legujka/plugins/luasnip.lua
@@ -2,7 +2,26 @@ return {
 	"L3MON4D3/LuaSnip",
 	version = "v2.*",
 	build = "make install_jsregexp",
-  dependences = {
+  dependencies = {
     "saadparwaiz1/cmp_luasnip",
-  }
+    "rafamadriz/friendly-snippets",
+    "honza/vim-snippets",
+  },
+  config = function()
+    require("luasnip.loaders.from_lua").lazy_load({
+      paths = {
+        vim.fn.stdpath("config") .. "/lua/legujka/snippets"
+      }
+    })
+
+    require("luasnip").setup({
+      update_events = "TextChanged,TextChangedI",
+      enable_autosnippets = true,
+      ext_opts = {
+        [require("luasnip.util.types").choiceNode] = {
+          active = { virt_text = { { "●", "DiagnosticWarn" } } }
+        }
+      }
+    })
+  end
 }
